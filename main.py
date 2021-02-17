d8b3df220be2367b2fc4b81c5261312726434738
Andrea <apratt1013@gmail.com>
2021-02-17 14:52:32 -0600

    add functions outline in main

--- main.py
+++ main.py
@@ -7,7 +7,25 @@ def main():
 
 def setup():
     artworkDB.create_database()
-    
+
+
+# def add_artist():
+
+
+# def search_by_artist():
+
+
+# def search_available_by_artist():
+
+
+# def add_artwork():
+
+
+# def delete_artwork():
+
+
+# def change_availability():
+
 
 
 if __name__ == "__main__":
