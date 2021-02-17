268616642046d600217dec86905f834d9d715dc6
Andrea <apratt1013@gmail.com>
2021-02-17 13:35:13 -0600

    add foreign key constraint and base model

--- artworkDB.py
+++ artworkDB.py
@@ -2,31 +2,29 @@ from peewee import *
 
 db = SqliteDatabase('Artwork.sqlite')
 
-class Artist(Model):
-    name = CharField(null=False, unique=True, constraints=[Check('length(name) >= 1'), Check('length(name) <= 30')])
-    email = CharField(null=False, constraints=[Check('length(email) >=1'), Check('length(email) <= 40')])
-
+class BaseModel(Model):
     class Meta:
         database = db
+
+class Artist(BaseModel):
+    name = CharField(null=False, unique=True, constraints=[Check('length(name) >= 1'), Check('length(name) <= 30')])
+    email = CharField(null=False, constraints=[Check('length(email) >=1'), Check('length(email) <= 40')])
 
 
     def __str__(self):
         return(f'Name: {self.name}, Email: {self.email}')
 
 
-class Artwork(Model):
-    artist = CharField(null=False, constraints=[Check('length(artist) >= 1'), Check('length(artist) <= 30')])
+class Artwork(BaseModel):
+    artist = ForeignKeyField(Artist, backref='name')
     name = CharField(null=False, constraints=[Check('length(name) >=1'), Check('length(name) <= 30')])
     price = DecimalField(null=False, constraints=[Check('price > 0'), Check('length(price) <= 5000')])
     available = BooleanField(null=False)
-
-    class Meta:
-        database = db
 
 
     def __str__(self):
         return(f'Artist: {self.artist}, Name of Artwork: {self.name}, Price: {self.price}, Available: {self.available}')
 
 
-db.connect()
-db.create_tables([Artist, Artwork])
+
+
