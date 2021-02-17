e826be388abed1d2cd2e92cbfd9c67b3b83cf034
Andrea <apratt1013@gmail.com>
2021-02-17 15:07:27 -0600

    add make_menu function in main module

--- main.py
+++ main.py
@@ -9,6 +9,17 @@ def setup():
     artworkDB.create_database()
 
 
+def make_menu():
+    menu = Menu()
+    menu.add_menu_option('1', 'Add new artist.', )
+    menu.add_menu_option('2', 'Search by artist.', )
+    menu.add_menu_option('3', 'Search available art by artist.', )
+    menu.add_menu_option('4', 'Add artwork.', )
+    menu.add_menu_option('5', 'Delete artwork.', )
+    menu.add_menu_option('6', 'Change artwork availability.', )
+    menu.add_menu_option('Q', 'Quit program.', )
+    return menu
+
 # def add_artist():
 
 
