d6fe11f8ccb221feb2849990d07bf9dd85b165fd
Andrea <apratt1013@gmail.com>
2021-02-20 16:19:08 -0600

    fix foreign key bug

--- artworkDB.py
+++ artworkDB.py
@@ -12,21 +12,19 @@ class Artist(BaseModel):
     name = CharField(null=False, unique=True, constraints=[Check('length(name) >= 1'), Check('length(name) <= 30')])
     email = CharField(null=False, unique=True, constraints=[Check('length(email) >=1'), Check('length(email) <= 40')])
 
-
     def __str__(self):
         return(f'Name: {self.name}, Email: {self.email}')
 
 
 class Artwork(BaseModel):
-    #artist = ForeignKeyField(Artist, backref='name')
+    artist = ForeignKeyField(Artist, backref='artworks')
     name = CharField(null=False, constraints=[Check('length(name) >=1'), Check('length(name) <= 30')])
     price = DecimalField(null=False, constraints=[Check('price > 0'), Check('length(price) <= 5000')])
     available = BooleanField(null=False)
 
 
     def __str__(self):
-        #return(f'Artist: {self.artist}, Name of Artwork: {self.name}, Price: {self.price}, Available: {self.available}')
-        print('hello')
+        return(f'Artist: {self.artist}, Name of Artwork: {self.name}, Price: {self.price}, Available: {self.available}')
 
 
 
@@ -56,8 +54,6 @@ def add_artwork(artist, name, price, availability):
 
 
 def add_artist(name, email):
-    print(name)
-    print(email)
     new_artist = Artist(name=name, email=email)
     new_artist.save()
  
