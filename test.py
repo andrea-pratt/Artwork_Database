98716773e4b4decb6e7c3656b190830634b15c85
Andrea <apratt1013@gmail.com>
2021-02-21 18:47:22 -0600

    add method in TestArtworkDB to add sample data to test database

--- test.py
+++ test.py
@@ -13,11 +13,26 @@ import artworkDB
 class TestArtworkDB(TestCase):
 
 
-    def setup(self):
+    def setUp(self):
         # Remove existing data from test database
         self.db = SqliteDatabase(db_path)
         self.db.drop_tables([Artist, Artwork])
         self.db.create_tables([Artist, Artwork])
+
+    def add_generic_sample_data(self):
+        artworkDB.add_artist('Bob', 'bob@gmail.com')
+        artworkDB.add_artist('Maria', 'maria@gmail.com')
+
+        artworkDB.add_artwork('Bob', 'Simplicity Defined', 3400.00, True)
+        artworkDB.add_artwork('Bob', 'Life Without Entropy', 8200.00, True)
+        artworkDB.add_artwork('Bob', 'Love in the Winter', 2200.00, False)
+
+        artworkDB.add_artwork('Maria', 'Waves Abound', 6700.00, False)
+        artworkDB.add_artwork('Maria', 'A Distant Mountain', 960.00, False)
+        artworkDB.add_artwork('Maria', 'In the End', 7600.00, True)
+
+
+       
 
 
 
